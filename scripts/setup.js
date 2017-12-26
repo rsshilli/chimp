573543ff178520fc5527f708afab0977b5c2e6b4
Sam Hatoum <sam@xolv.io>
2017-12-26 18:16:30 -0500

    Remove usesless warn log setter as Circle is ignoring it

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const packageJson = require('../package.json')
 const exec = require('./lib/exec')
-exec(`npm config set loglevel warn `)
+
 exec(`npm run transpile `)
 exec(`cd ${process.cwd()}/${packageJson.chimpDir} && npm i`)
 if (process.env.CIRCLECI) {
