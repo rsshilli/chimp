d4e7f618c28aeb75afadff351d11225be823924f
Sam Hatoum <sam@xolv.io>
2017-12-26 23:23:53 -0500

    Remove chrome installation

--- .circleci/get-latest-chrome-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo $(wget -qO- https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm | head -c96 | strings | rev | awk -F"[:-]" '/emorhc/ { print $1 "-" $2 }' | rev) > ~/chrome-version
-echo Latest Chrome version is `cat ~/chrome-version`
