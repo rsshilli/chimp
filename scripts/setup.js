247695e382448069ba0f3a7a3cca77a79cd9638f
Sam Hatoum <sam@xolv.io>
2017-12-26 22:49:00 -0500

    Single liner refactor

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,12 +2,8 @@
 const packageJson = require('../package.json')
 const exec = require('./lib/exec')
 
-exec(`npm run transpile `)
+exec(`npm run transpile`)
 exec(`cd ${process.cwd()}/${packageJson.chimpDir} && npm i`)
-if (process.env.CIRCLECI) {
-  exec(`cd ${process.cwd()}/${packageJson.distDir} && sudo npm link`)
-} else {
-  exec(`cd ${process.cwd()}/${packageJson.distDir} && npm link`)
-}
+exec(`cd ${process.cwd()}/${packageJson.distDir} && ${process.env.CIRCLECI ? 'sudo' : ''} npm link`)
 exec(`cd ${process.cwd()}/${packageJson.distDir} && npm i`)
 exec(`cd ${process.cwd()}/${packageJson.testProjectDir} && npm i`)
