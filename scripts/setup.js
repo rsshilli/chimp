1ddc40f1f4ca5f81bc802535a239b61affb46358
Sam Hatoum <sam@xolv.io>
2017-12-26 23:52:42 -0500

    Speeds up chimp setup locally (a smidgen)

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,9 +3,9 @@ const packageJson = require('../package.json')
 const exec = require('./lib/exec')
 
 exec(`npm run transpile`)
-if (!process.env.CIRCLECI) {
-  exec(`cd ${process.cwd()}/${packageJson.chimpDir} && npm i`)
-}
 exec(`cd ${process.cwd()}/${packageJson.distDir} && ${process.env.CIRCLECI ? 'sudo' : ''} npm link`)
+if (!process.env.CIRCLECI) {
+  exec(`cd ${process.cwd()}/${packageJson.chimpDir} && npm i`, {async: true})
+}
 exec(`cd ${process.cwd()}/${packageJson.distDir} && npm i`, {async: true})
 exec(`cd ${process.cwd()}/${packageJson.testProjectDir} && npm i`, {async: true})
