f10cf6846ccd0ffa8177fcd1d46cb58ae66b89c3
Sam Hatoum <sam@xolv.io>
2017-12-26 22:49:13 -0500

    Fix posix if blocks

--- .circleci/install-chrome.sh
+++ .circleci/install-chrome.sh
@@ -12,7 +12,7 @@ echo Latest version of Chrome is $LATEST_CHROME_VERSION_MAJOR.$LATEST_CHROME_VER
 
 # -- CHECK IF CHROME IS INSTALLED
 INSTALL_CHROME=false
-if [ -f /opt/google/chrome/google-chrome ]; then
+if [[ -f /opt/google/chrome/google-chrome ]]; then
       # GET INSTALLED CHROME VERSION MAJOR & MINOR NUMBERS
       INSTALLED_CHROME_VERSION=$(/opt/google/chrome/google-chrome --version)
       INSTALLED_CHROME_VERSION=${INSTALLED_CHROME_VERSION/Google Chrome /}
@@ -20,9 +20,9 @@ if [ -f /opt/google/chrome/google-chrome ]; then
       INSTALLED_CHROME_VERSION_MINOR=`echo $INSTALLED_CHROME_VERSION | cut -d. -f2`
       echo Installed version of Chrome is $INSTALLED_CHROME_VERSION_MAJOR.$INSTALLED_CHROME_VERSION_MINOR
 
-  echo comparing [ $LATEST_CHROME_VERSION_MAJOR > $INSTALLED_CHROME_VERSION_MAJOR ] || [ $LATEST_CHROME_VERSION_MINOR > $INSTALLED_CHROME_VERSION_MINOR ]
+  echo comparing [ $LATEST_CHROME_VERSION_MAJOR -gt $INSTALLED_CHROME_VERSION_MAJOR ] || [ $LATEST_CHROME_VERSION_MINOR -gt $INSTALLED_CHROME_VERSION_MINOR ]
   # -- COMPARE INSTALLED AND LATEST VERSIONS
-  if [ $LATEST_CHROME_VERSION_MAJOR -gt $INSTALLED_CHROME_VERSION_MAJOR ] || [ $LATEST_CHROME_VERSION_MINOR -gt $INSTALLED_CHROME_VERSION_MINOR ]; then
+  if [[ $LATEST_CHROME_VERSION_MAJOR -gt $INSTALLED_CHROME_VERSION_MAJOR ]] || [[ $LATEST_CHROME_VERSION_MINOR -gt $INSTALLED_CHROME_VERSION_MINOR ]]; then
     echo Latest Chrome version is newer than installed version.
     DO_INSTALL=true;
   else
@@ -33,7 +33,7 @@ else
   INSTALL_CHROME=true;
 fi
 
-if [ $INSTALL_CHROME == true ]; then
+if [[ $INSTALL_CHROME == true ]]; then
   echo Installing Chrome $LATEST_CHROME_VERSION_MAJOR.$LATEST_CHROME_VERSION_MINOR
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg -i google-chrome-stable_current_amd64.deb # this gives information to apt-get about missing dependencies
